49960cc40ad59878d43490bc8a9e2449f946b4e2
sivenz <meutszz@gmail.com>
2025-09-15 16:35:03 -0300

    conclusão das primeiras atividades

---
+++ exercicios/atv6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int segundos, h, m, s, resto;
+
+    printf("Digite uma quantidade de segundos: ");
+    scanf("%d", &segundos);
+
+    h = segundos / 60;
+
+    return 0;
+}
